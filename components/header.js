adcc7e2fc6144c4de00f7fdb207a88d24c4010eb
kyoungjooo <leekjoo1008@gmail.com>
2025-09-10 18:10:41 +0900

    style: main

---
+++ components/header.js
@@ -0,0 +1,21 @@
+const template = `
+<header class="header header--sticky">
+  <div class="header__inner container--xl">
+    <a href="/" class="header__logo background"><h1 class="hidden">logo</h1></a>
+    <nav class="header__nav" aria-label="Main">
+      <a href="/about.html"   class="header__link">Who We are</a>
+      <a href="/adopt.html"   class="header__link">What We Do</a>
+      <a href="/contact.html" class="header__link">Core Technology</a>
+      <a href="/contact.html" class="header__link">Pipelines</a>
+      <a href="/contact.html" class="header__link">Achievements</a>
+      <a href="/contact.html" class="header__link">Contact Us</a>
+    </nav>
+  </div>
+</header>`;
+
+class SiteHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = template;
+  }
+}
+customElements.define("site-header", SiteHeader);
