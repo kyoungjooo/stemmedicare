63e5722e5fe2e45afa6d588b9d66d0bd2791f467
kyoungjooo <leekjoo1008@gmail.com>
2025-09-12 16:53:30 +0900

    style: 베너 공통

---
+++ components/banner.js
@@ -0,0 +1,34 @@
+const template = ({ title, descHtml, sectionClass = "" }) => `
+    <section class="page-banner background ${sectionClass}">
+      <div class="page-banner__inner container--md container">
+        <div class="page-banner__content">
+          <h2 class="page-banner__title font-teko">${title}</h2>
+          <p class="page-banner__desc">
+           ${descHtml}
+          </p>
+        </div>
+      </div>
+    </section>`;
+
+class PageBanner extends HTMLElement {
+  static get observedAttributes() {
+    return ["title", "desc", "section-class"];
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+  attributeChangedCallback() {
+    this.render();
+  }
+
+  render() {
+    const title = this.getAttribute("title") || "";
+    const rawDesc = this.getAttribute("desc") || "";
+    const descHtml = rawDesc.replace(/\\n|\n/g, "<br>");
+    const sectionClass = this.getAttribute("section-class");
+    this.innerHTML = template({ title, descHtml, sectionClass });
+  }
+}
+
+customElements.define("page-banner", PageBanner);
