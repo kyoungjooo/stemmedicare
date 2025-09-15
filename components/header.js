99d06fa07badd7746d3fa9ea6084f1f702a7f88d
kyoungjooo <leekjoo1008@gmail.com>
2025-09-15 15:30:27 +0900

    fix

--- components/header.js
+++ components/header.js
@@ -3,12 +3,12 @@ const template = `
   <div class="header__inner container--xl container">
     <a href="/" class="header__logo background"><h1 class="hidden">logo</h1></a>
     <nav class="header__nav" aria-label="Main">
-      <a href="/"   class="header__link">Who We are</a>
-      <a href="/"   class="header__link">What We Do</a>
+      <a href="/core-overview.html"   class="header__link">Who We are</a>
+      <a href="/core-overview.html"   class="header__link">What We Do</a>
       <a href="/core-overview.html" class="header__link">Core Technology</a>
-      <a href="/" class="header__link">Pipelines</a>
-      <a href="/" class="header__link">Achievements</a>
-      <a href="/" class="header__link">Contact Us</a>
+      <a href="/core-overview.html" class="header__link">Pipelines</a>
+      <a href="/core-overview.html" class="header__link">Achievements</a>
+      <a href="/core-overview.html" class="header__link">Contact Us</a>
     </nav>
   </div>
 </header>`;
